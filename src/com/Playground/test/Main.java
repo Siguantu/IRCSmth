b0527257f95a2f7879f0262b28ab4146debcc276
augi mach <siguantu@gmail.com>
2020-03-04 21:02:27 +0100

    First commit

--- src/com/Playground/test/Main.java
+++ src/com/Playground/test/Main.java
@@ -9,9 +9,9 @@ public class Main {
     public static void main (String[] args) throws IOException {
         IRCTwitch client = new IRCTwitch("irc.chat.twitch.tv", IRCInterface.IRCDefaultPort);
         client.IRCLogin("Name", "oauth:YourOAuth");
-        client.IRCJoinChannel("#siguantu");
-        client.IRCSendChannelMessage("Test1", "#siguantu");
-        client.IRCSendChannelMessage("Test2", "#siguantu");
+        client.IRCJoinChannel("#channelname");
+        client.IRCSendChannelMessage("Test1", "#channelname");
+        client.IRCSendChannelMessage("Test2", "#channelname");
         client.IRCDisconnect();
     }
 }
